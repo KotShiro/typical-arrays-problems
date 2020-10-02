5100ce69daa73dea13cca831bf290ec55753ac64
Никита А. Ковалевич <C:\Users\kovalevich\AppData\Roaming\The Bat!>
2020-10-02 11:47:47 +0300

    refactor: index.js typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
-exports.min = function min (array) {
+location = function location (way, array) {
+  if (array && array.length !== 0 && Array.isArray(array)){
+    let dump = array[0];
+    for (let i = 0; i < array.length; i += 1) {
+      if (way === 'min') {
+        if (array[i] < dump) dump = array[i];
+      }
+      if (way === 'max') {
+        if (array[i] > dump) dump = array[i];
+      }
+    }
+    return dump;
+  }
   return 0;
+}
+
+exports.min = function min (array) {
+  return location('min',array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  return location('max',array);
 }
 
 exports.avg = function avg (array) {
+  if (array && array.length !== 0 && Array.isArray(array)){
+    let dump = 0;
+    array.forEach(function(item) {
+      dump += item;
+    });
+    return dump/array.length;
+  }
   return 0;
 }
